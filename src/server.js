1a6815891d2dcde4d5d43f452f4d753ecd541b4c
vancuong11 <cuongg121101@gmail.com>
2023-09-11 16:40:53 +0700

    #1 Xây dựng thư mục và connect mongo

---
+++ src/server.js
@@ -0,0 +1,31 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+dotenv.config();
+
+const app = express();
+const port = process.env.PORT || 8888;
+
+// config mongoDB
+mongoose
+    .connect(process.env.MONGO_DB)
+    .then(() => {
+        console.log('connection DB successfully');
+    })
+    .catch((err) => {
+        console.log(err);
+    });
+
+// config bodyParser
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// run app
+app.get('/', (req, res) => {
+    res.send('server on success');
+});
+
+app.listen(port, () => {
+    console.log('server listening on port ' + port);
+});
